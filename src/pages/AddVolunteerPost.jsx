ae9a69562939a9552f23ce6ffb51f6224687c48c
Mejbahul-Hasan <mejba.du@gmail.com>
2024-05-11 18:15:21 +0600

    React datepicker is installed

--- src/pages/AddVolunteerPost.jsx
+++ src/pages/AddVolunteerPost.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
 
 const AddVolunteerPost = () => {
+
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
         <div className="bg-gray-200 rounded-2xl mt-8 text-center max-w-6xl mx-auto">
             <h1 className="text-3xl py-5">Please fill-up the following form:</h1>
@@ -19,7 +27,7 @@ const AddVolunteerPost = () => {
                     </label>
                 </div>
                 <div className="lg:flex gap-5">
-                <label className="form-control w-full gap-2 m-5 md:w-1/2">
+                    <label className="form-control w-full gap-2 m-5 md:w-1/2">
                         <div className="label">
                             <span className="label-text font-bold">Post Title</span>
                         </div>
@@ -53,19 +61,19 @@ const AddVolunteerPost = () => {
                         </div>
                         <input type="text" placeholder="Number of Volunteers Needed to Accomplish the service" className="input input-bordered max-w-md" />
                     </label>
-                    <label className="form-control w-full gap-2 m-5 md:w-1/2">
+                    <label className="form-control w-full gap-2 m-5 md:w-1/2 items-start">
                         <div className="label">
                             <span className="label-text font-bold">Deadline</span>
                         </div>
-                        <input type="text" placeholder="Healthcare/Education/Social Service/Animal Welfare" className="input input-bordered max-w-md" />
+                        <DatePicker className="border py-3 rounded-xl px-32" selected={startDate} onChange={(date) => setStartDate(date)} />
                     </label>
                 </div>
                 <label className="form-control w-full gap-2 m-5">
-                        <div className="label">
-                            <span className="label-text font-bold">Thumbnail</span>
-                        </div>
-                        <input type="text" placeholder="Thumbnail/image URL" className="input input-bordered max-w-md lg:max-w-5xl" />
-                    </label>
+                    <div className="label">
+                        <span className="label-text font-bold">Thumbnail</span>
+                    </div>
+                    <input type="text" placeholder="Thumbnail/image URL" className="input input-bordered max-w-md lg:max-w-5xl" />
+                </label>
                 <button className="btn w-1/2 my-5">Add Post</button>
             </form>
         </div>
